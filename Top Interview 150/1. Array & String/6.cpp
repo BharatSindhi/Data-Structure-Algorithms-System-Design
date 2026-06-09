6688e799b643aff9c39442d6fceda4ebff44627d
BharatSindhi <sindhibharat567@gmail.com>
2026-06-09 13:36:18 +0530

    solve an dsa problem

---
+++ Top Interview 150/1. Array & String/6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> myMap = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int result = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() &&
+                myMap[s[i]] < myMap[s[i + 1]]) {
+                result -= myMap[s[i]];
+            } else {
+                result += myMap[s[i]];
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    // your code here
+    return 0;
+}
