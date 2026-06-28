411ee420c3d3f9949df7b5b09022fad85e72944a
BharatSindhi <sindhibharat567@gmail.com>
2026-06-28 21:27:45 +0530

    solve an dsa problem

---
+++ Top Interview 150/Two Pointers/2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0; // pointer for s
+        int j = 0; // pointer for t
+
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.size();
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
