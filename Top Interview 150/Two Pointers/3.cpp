95b2cadc48dbc359d6089e81875f953bbe0485d0
BharatSindhi <sindhibharat567@gmail.com>
2026-07-04 12:03:08 +0530

    solve dsa problem

---
+++ Top Interview 150/Two Pointers/3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int l = 0;
+        int r = n - 1;
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+
+            if (sum == target) {
+                return {l + 1, r + 1};
+            } 
+            else if (sum > target) {
+                r--;
+            } 
+            else {
+                l++;
+            }
+        }
+
+        return {};
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
