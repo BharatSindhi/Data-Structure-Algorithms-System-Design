7443ac136bed5710bd414d756b87bc0dfeea6de3
BharatSindhi <sindhibharat567@gmail.com>
2026-07-06 13:33:39 +0530

    solve an dsa problem

---
+++ Top Interview 150/2. Binary Search/4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int st=0;
+        int end=n-1;
+
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[st]<=nums[mid]){ // left sorted
+                if(nums[st]<=target && target<=nums[mid]){
+                    end=mid-1;
+                }else{
+                    st=mid+1;
+                }
+            }else{//right sorted
+                if(nums[mid]<=target && target<=nums[end]){
+                    st=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+int main() {
+    
+    return 0;
+}
