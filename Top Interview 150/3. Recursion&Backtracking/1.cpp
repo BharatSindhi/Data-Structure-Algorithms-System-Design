574aebe0a813e8c4328dcd9b646a6991919154a9
BharatSindhi <sindhibharat567@gmail.com>
2026-07-13 13:44:58 +0530

    solve an dsa problem

---
+++ Top Interview 150/3. Recursion&Backtracking/1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/solutions/5601412/easy-and-simple-c-solution-detailed-expl-oe7i
+class Solution {
+private:
+    void solve(string digit, string output, int i, vector<string>& ans,
+               string mapping[]) {
+        if (i >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+
+        int num = digit[i] - '0';
+        string value = mapping[num];
+
+        for (int j = 0; j < value.length(); j++) {
+            output.push_back(value[j]);
+            solve(digit, output, i + 1, ans, mapping);
+            output.pop_back();
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digit) {
+        vector<string> ans;
+        if (digit.length() == 0) {
+            return ans;
+        }
+        int i = 0;
+        string output;
+        string mapping[10] = {"",    "",    "abc",  "def", "ghi",
+                              "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        solve(digit, output, i, ans, mapping);
+        return ans;
+    }
+};
+int main() {
+    
+    return 0;
+}
