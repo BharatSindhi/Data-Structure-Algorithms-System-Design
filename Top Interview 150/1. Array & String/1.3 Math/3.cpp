61d3e1d30ed771e3e14cbd2697518ac1966f07ee
BharatSindhi <sindhibharat567@gmail.com>
2026-08-02 17:24:20 +0530

    solve an dsa problem

---
+++ Top Interview 150/1. Array & String/1.3 Math/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) { 
+        int count = 0;
+        for (long long i = 5; n / i; i *= 5)
+            count += n / i;
+        return count;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
